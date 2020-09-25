fa845478035f47adf162976a75ad70d304ad6d59
Yvonne Bohan <Yvonne Bohan>
2020-09-25 14:24:25 -0400

    Add project files.

---
+++ While-example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = 1;
+            while (a < 26) {
+                var cubed = a * a * a;
+                Console.WriteLine($"{a} cubed {cubed}");
+                //a++;
+
+
+            }
+            
+        }
+    }
+}
